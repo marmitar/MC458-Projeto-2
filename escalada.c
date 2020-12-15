f6066ecfc17c6b16b60fc4d2686acba2d47a30a4
Tiago de Paula <tiagodepalves@gmail.com>
2020-12-14 21:49:58 -0300

    Inicial

---
+++ escalada.c
@@ -0,0 +1,19 @@
+/**
+ * Este arquivo pode ser usado como base para o seu codigo.
+ * Preste especial atencao a funcao ja implementada "imprimeSaida". Esta função
+ * deve ser usada para imprimir o resultado da execucao de cada algoritmo.
+ */
+
+#include <stdio.h>
+
+/**
+ * Imprime o custo do caminho.
+ */
+void imprimeSaida(int custo) {
+    printf("%d\n", custo);
+}
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}
---
+++ escalada.cpp
@@ -0,0 +1,25 @@
+/**
+ * Este arquivo pode ser usado como base para o seu codigo.
+ * Preste especial atencao a funcao ja implementada "imprimeSaida". Esta função
+ * deve ser usada para imprimir o resultado da execucao de cada algoritmo.
+ */
+
+#include <stdio.h>
+#include <vector>
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Imprime o custo do caminho.
+ */
+void imprimeSaida(int custo) {
+    cout << custo << endl;
+}
+
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}
