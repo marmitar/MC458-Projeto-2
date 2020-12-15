832603977f1b34e6c37a00598b418d8d64f8e278
Tiago de Paula <tiagodepalves@gmail.com>
2020-12-14 22:18:25 -0300

    Descrição da parede

--- escalada.c
+++ escalada.c
@@ -5,15 +5,35 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+
+#define attribute(...) \
+    __attribute__((__VA_ARGS__))
+
+typedef uint32_t custo_t;
+
+typedef struct parede {
+    size_t n, m;
+    uint8_t *dados;
+} parede_t;
+
+static inline attribute(pure)
+custo_t custo_em(const parede_t parede, size_t i, size_t j) {
+    size_t pos = i * parede.n + j;
+    return (custo_t) parede.dados[pos];
+}
 
 /**
  * Imprime o custo do caminho.
  */
-void imprimeSaida(int custo) {
-    printf("%d\n", custo);
+void imprimeSaida(custo_t custo) {
+    printf("%" PRIu32 "u\n", custo);
 }
 
 int main(int argc, char const *argv[]) {
 
-    return 0;
+    return EXIT_FAILURE;
 }
